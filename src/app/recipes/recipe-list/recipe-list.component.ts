43177cb4d5edf41716e78d3edcc38fb6ebb17baf
Gerardo Romero <gromero@sitek.mx>
2017-06-27 15:04:37 -0600

    Displaying Recipe Details

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes:Array<Recipe> = [ new Recipe('R1','Description 1', 'https://static.pexels.com/photos/2232/vegetables-italian-pizza-restaurant.jpg'),
+                            new Recipe('R2','Description 2', 'https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2016/12/19/18/sush0istock-gkrphoto.jpg')
+                          ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
