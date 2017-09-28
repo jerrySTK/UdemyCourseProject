7a93b330c3e86bbfc75b13125495d269389f24fe
Gerardo Romero <gromero@sitek.mx>
2017-09-28 11:37:16 -0600

    Directives

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,27 @@
+import { Directive,OnInit,ElementRef,HostListener,Renderer2 } from '@angular/core';
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective implements OnInit{
+    
+    private opened:boolean = false;
+
+
+    constructor(private element: ElementRef,private renderer: Renderer2){
+
+    }
+
+    ngOnInit(){
+
+    }
+
+    @HostListener('click') onclick(eventData: Event){
+        this.opened = !this.opened;
+        if(this.opened)
+            this.renderer.addClass(this.element.nativeElement,'open');
+        else
+            this.renderer.removeClass(this.element.nativeElement,'open');
+    }
+
+}
